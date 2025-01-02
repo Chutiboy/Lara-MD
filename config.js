41dde14d0a9d47c80eb9dfaceeed3b3f93f93b4d
Chutiboy <152064588+Chutiboy@users.noreply.github.com>
2025-01-02 13:40:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "bzBRTZyR#BZcCii33sfCeC0ep7ITunPL3UgwCngvTwq942eh0a5c", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "emYFBDwI#c9bIyFkukiNsFLxODaDf9IMDx8HJBwGc6Llzxb6Xrww", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
